bf13f0625223237bb6d6e5c5905f302e7ae7e7f4
asab63 <alaa.sab63@gmail.com>
2023-04-10 12:14:01 +0300

    some changes in login page

--- lib/view/screen/auth/login.dart
+++ lib/view/screen/auth/login.dart
@@ -1,7 +1,7 @@
+import 'package:ahmet_usta/view/widget/auth/customtextbodyauth.dart';
 import 'package:ahmet_usta/view/widget/auth/customtextformauth.dart';
+import 'package:ahmet_usta/view/widget/auth/customtexttitleauth.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-
 class Login extends StatelessWidget {
   const Login({super.key});
 
@@ -16,27 +16,19 @@ class Login extends StatelessWidget {
       ),
       body: Container(
         padding: const EdgeInsets.symmetric(vertical: 15,horizontal: 30),
-        child: ListView(children: [
-          const SizedBox(height: 25),
-          Text("HOŞGELDİNİZ",
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.displayMedium),
-          const SizedBox(height: 20),
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 25) ,
-            child: Text("E-Postanız Ve Şifrenizle Giriş Yapın Veya Sosyal Medya İle Devam Edin",
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge,),
-          ),
-         const SizedBox(height: 65),
-         const CustomTextFormAuth(
+        child: ListView(children: const [
+        SizedBox(height: 25),
+        CustomTextTitleAuth(text:"HOŞGELDİNİZ"),
+        CustomTextBodyAuth(text: "E-Postanız Ve Şifrenizle Giriş Yapın Veya Sosyal Medya İle Devam Edin"),
+        SizedBox(height: 65),
+        CustomTextFormAuth(
           hintText: "E-postanızı giriniz",
           labeltext: "Email",
           iconData: Icons.email_outlined,
           //mycontroller: mycontroller
           ),
-          const SizedBox(height: 25),
-          const CustomTextFormAuth(
+          SizedBox(height: 25),
+          CustomTextFormAuth(
           hintText: "Şifrenizi giriniz",
           labeltext: "Şifre",
           iconData: Icons.lock_outlined,
