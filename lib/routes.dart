7624606e77e6542ebbb5dc739aacedf32f787ac9
asab63 <alaa.sab63@gmail.com>
2023-04-10 17:33:12 +0300

    login and signup pages

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,17 @@
 import 'package:ahmet_usta/core/constant/routes.dart';
+import 'package:ahmet_usta/view/screen/auth/forgetpassword.dart';
 import 'package:ahmet_usta/view/screen/auth/login.dart';
+import 'package:ahmet_usta/view/screen/auth/resetpassword.dart';
+import 'package:ahmet_usta/view/screen/auth/signup.dart';
+import 'package:ahmet_usta/view/screen/auth/verfiycode.dart';
+import 'package:ahmet_usta/view/screen/onboarding.dart';
 import 'package:flutter/material.dart';
-import 'package:path/path.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
- AppRoute.login : (context) => const Login()
+  AppRoute.login: (context) => const Login(),
+  AppRoute.signup: (context) => const SignUp(),
+  AppRoute.forgetPassword: (context) => const ForgetPassword(),
+  AppRoute.verfiyCode: (context) => const VerfiyCode(),
+  AppRoute.resetPassword: (context) => const ResetPassword(),
+  AppRoute.onboarding: (context) => const OnBoarding(),
 };
